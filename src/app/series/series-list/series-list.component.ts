4e703a765743aa45f1879d8f4ae4d89d1e1344c9
Juan D. Uribe <jd.uribe2@uniandes,edu.co>
2025-05-14 16:30:36 -0500

    .

--- src/app/series/series-list/series-list.component.ts
+++ src/app/series/series-list/series-list.component.ts
@@ -13,14 +13,21 @@ import { Series } from '../series';
 export class SeriesListComponent implements OnInit {
 
   series: Series[] = [];
+  averageSeasons: number = 0;
 
   constructor(private seriesService: SeriesService) { }
 
   ngOnInit() {
     this.seriesService.getSeries().subscribe((data) => {
       this.series = data;
+      this.averageSeasons = this.calculateAverageSeasons();
     });
   }
+
+    calculateAverageSeasons(): number {
+      const total = this.series.reduce((sum, serie) => sum + serie.seasons, 0);
+      return total / this.series.length;
+    }
 
   selectedSerie: Series | null = null;
 
